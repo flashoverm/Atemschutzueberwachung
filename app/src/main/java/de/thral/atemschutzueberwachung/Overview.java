d7715b7729339617f7253e465bb841b2e36a02be
flashoverm <markus@thral.de>
2017-10-25 23:06:10 +0200

    Tested different layouts

---
+++ app/src/main/java/de/thral/atemschutzueberwachung/Overview.java
@@ -0,0 +1,15 @@
+package de.thral.atemschutzueberwachung;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Overview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+
+    }
+}
