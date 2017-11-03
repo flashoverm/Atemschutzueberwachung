3606223fe8894d5a2cd4790582b0dcaa76aefa54
flashoverm <markus@thral.de>
2017-11-04 00:02:52 +0100

    Version 0.4.1: Added Toasts if necessary fields in dialogs not filled in

---
+++ app/src/main/java/de/thral/atemschutzueberwachung/persistence/DraegermanDAOImpl.java
@@ -0,0 +1,31 @@
+package de.thral.atemschutzueberwachung.persistence;
+
+import java.util.List;
+
+import de.thral.atemschutzueberwachung.domain.Draegerman;
+
+/**
+ * Created by Markus Thral on 01.11.2017.
+ */
+
+public class DraegermanDAOImpl implements DraegermanDAO {
+
+    public DraegermanDAOImpl(){
+
+    }
+
+    @Override
+    public List<Draegerman> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean add(Draegerman draegerman) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Draegerman draegerman) {
+        return false;
+    }
+}
